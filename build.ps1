d203017e237bdbc4275a20b3b45a2c79c7bb7d60
BobbyShmurner <glitched.ohdeymus@gmail.com>
2022-08-19 19:01:27 +0100

    Updated build script

--- build.ps1
+++ build.ps1
@@ -35,26 +35,31 @@ if ($env:HOME -ne "/home/bobby" -or $env:CAN_RUN_ULTRAINTERFACE_BUILD_SCRIPT -ne
 }
 
 echo "`n- Removing Files"
-rm -rf ./ULTRAINTERFACE/Package/contentFiles/any/any/resources/*
-rm -rf ./ULTRAINTERFACE/Package/contentFiles/any/any/src/*
+
 rm -rf ~/Documents/dev/NuGetSource/ultrainterface/0.0.1/
 rm -rf /home/bobby/.nuget/packages/ultrainterface/
+rm -rf ./ULTRAINTERFACE/Package/contentFiles/
 
 rm -f ./ULTRAINTERFACE/Package/ULTRAINTERFACE.nupkg
 rm -f ./ULTRAINTERFACE/resources/ultrainterface
 
+echo "- Making Directories"
+
+mkdir -p ./ULTRAINTERFACE/Package/contentFiles/any/any/resources/
+mkdir -p ./ULTRAINTERFACE/Package/contentFiles/any/any/src/
+
 echo "- Copying Files"
 
-cp /ssd/Unity/Projects/ULTRAINTERFACE/Assets/StreamingAssets/ultrainterface ./ULTRAINTERFACE/resources/
+cp ./UnityProject/Assets/StreamingAssets/ultrainterface ./ULTRAINTERFACE/resources/
 cp -a ./ULTRAINTERFACE/resources/. ./ULTRAINTERFACE/Package/contentFiles/any/any/resources/
 cp -a ./ULTRAINTERFACE/src/. ./ULTRAINTERFACE/Package/contentFiles/any/any/src/
 
-echo "- Creating and Installing NuGet Package`n"
+echo "- Creating and Installing NuGet Package: `n"
 
 nuget pack ULTRAINTERFACE/Package/ULTRAINTERFACE.nuspec -OutputDirectory ./ULTRAINTERFACE/Package/ -OutputFileNamesWithoutVersion
 nuget add ULTRAINTERFACE/Package/ULTRAINTERFACE.nupkg -Source ~/Documents/dev/NuGetSource
 
-echo "`n- Building Example Mod`n"
+echo "`n- Building Example Mod: `n"
 
 dotnet build ./ExampleUI/ExampleUI.csproj
 $buildExitCode = $LASTEXITCODE
@@ -65,6 +70,11 @@ $cwd = (Get-Location).path
 
 rm -rf /home/bobby/.nuget/packages/ultrainterface/0.0.1/contentFiles/any/any/src/
 ln -s $cwd/ULTRAINTERFACE/src ~/.nuget/packages/ultrainterface/0.0.1/contentFiles/any/any/
+
+echo "- Cleaning up"
+
+rm -rf ./ULTRAINTERFACE/Package/contentFiles/
+rm -f ./ULTRAINTERFACE/Package/ULTRAINTERFACE.nupkg
 
 echo "- Copying Example Mod to Scripts Folder"
 
@@ -82,7 +92,7 @@ if ($buildExitCode -ne 0) {
 
 $process = Get-Process "ULTRAKILL.exe" -ErrorAction SilentlyContinue
 if (-not $process) {
-	echo "`n- Launching ULTRAKILL`n"
+	echo "`n- Launching ULTRAKILL:`n"
 	steam steam://rungameid/1229490
 	echo "`n- Lanched ULTRAKILL!"
 }
