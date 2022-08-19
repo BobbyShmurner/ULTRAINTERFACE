6fc7b65359768a5b72bb0e099356672e0bc720d3
BobbyShmurner <glitched.ohdeymus@gmail.com>
2022-08-19 18:22:23 +0100

    Added a flag to Rebuild for updating navigation

---
+++ ULTRAINTERFACE/src/Components/OptionsMenu.cs
@@ -0,0 +1,138 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+using System;
+using System.Collections.Generic;
+
+namespace ULTRAINTERFACE {
+	public class OptionsMenu : UIComponent {
+		public List<Action<OptionsMenu>> LateCreate { get; private set; } = new List<Action<OptionsMenu>>();
+		public List<Action<OptionsMenu>> FirstShown { get; private set; } = new List<Action<OptionsMenu>>();
+		public List<Action<OptionsMenu>> OnShown { get; private set; } = new List<Action<OptionsMenu>>();
+
+		public CustomScrollView ScrollView { get; private set; }
+		public CustomButton OptionsButton { get; private set; }
+		public CustomText Title { get; private set; }
+
+		public RectTransform Content { get { return ScrollView.Content; } }
+
+		public bool IsInitalised { get; private set; } = false;
+		public bool HasBeenShown { get; private set; } = false;
+
+		float previousHeight;
+
+		public void ScrollToTop(bool includeChildren = true) {
+			if (!includeChildren) {
+				ScrollView.ScrollRect.ScrollToTop();
+				return;
+			}
+
+			foreach (ScrollRect scrollRect in GetComponentsInChildren<ScrollRect>(true)) {
+				scrollRect.ScrollToTop();
+			}
+		}
+
+		public RectTransform AddOptionsPanel(TextAnchor anchor = TextAnchor.MiddleLeft) {
+			return Options.CreateOptionsPanel(Content, anchor);
+		}
+
+		public CustomText AddHeader(string text, int size = 24, float width = 600, float height = 50, TextAnchor anchor = TextAnchor.MiddleCenter, bool forceCaps = true) {
+			return UI.CreateText(Content, text, size, width, height, anchor, forceCaps);
+		}
+
+		public CustomButton AddButton(string text, UnityAction onClick, int fontSize = 24, float width = 600, float height = 60, bool forceCaps = true) {
+			return UI.CreateButton(Content, text, onClick, fontSize, width, height, forceCaps);
+		}
+
+		public CustomButton AddButton(string text, int fontSize = 24, float width = 600, float height = 60, bool forceCaps = true) {
+			return UI.CreateButton(Content, text, fontSize, width, height, forceCaps);
+		}
+
+		public void Rebuild(bool updateNavigation = true) {
+			Content.Rebuild(updateNavigation);
+
+			// Content.anchoredPosition = new Vector2(Content.anchoredPosition.x, Content.anchoredPosition.y - (Content.sizeDelta.y - previousHeight) / 2);
+
+			// This is just here until the new nav system is ready
+			if (updateNavigation) UpdateNavigation();
+		}
+
+		void LateUpdate() {
+			previousHeight = Content.sizeDelta.y;
+		}
+
+		internal void Init(CustomScrollView scrollView, CustomButton optionsButton, CustomText title) {
+			if (IsInitalised) {
+				UI.Log.LogError($"Options Menu \"{gameObject.name}\" already initalised, returning...");
+				return;
+			}
+
+			this.ScrollView = scrollView;
+			this.OptionsButton = optionsButton;
+			this.Title = title;
+
+			this.HasBeenShown = false;
+
+			CoroManager.InvokeNextFrame(() => {
+				foreach (Action<OptionsMenu> action in LateCreate) {
+					action(this);
+				}
+				
+				Rebuild();
+			});
+
+			IsInitalised = true;
+		}
+
+		void UpdateNavigation() {
+			Selectable[] selectables = ScrollView.Content.GetComponentsInChildren<Selectable>(false);
+
+			for (int i = 0; i < selectables.Length; i++) {
+				Selectable selectable = selectables[i];
+
+				Navigation nav = new Navigation();
+ 				nav.mode = Navigation.Mode.Explicit;
+
+				if (i > 0) {
+					nav.selectOnUp = selectables[i - 1];
+				} else {
+					nav.selectOnUp = selectables[selectables.Length - 1];
+				}
+				if (i < selectables.Length - 1) {
+					nav.selectOnDown = selectables[i + 1];
+				} else {
+					nav.selectOnDown = selectables[0];
+				}
+
+				selectable.navigation = nav;
+			}
+		}
+
+		public void SetTitle(string titleText, bool forceCaps = true) {
+			Title.SetText($"--{titleText}--", forceCaps);
+		}
+
+		public void SetButtonText(string buttonText, bool forceCaps = true) {
+			OptionsButton.Text.SetText(buttonText, forceCaps);
+		}
+
+		void OnEnable() {
+			if (!HasBeenShown) {
+				HasBeenShown = true;
+
+				CoroManager.InvokeNextFrame(() => {
+					foreach (Action<OptionsMenu> action in FirstShown) {
+						action(this);
+					}
+				});
+			}
+
+			CoroManager.InvokeNextFrame(() => {
+				foreach (Action<OptionsMenu> action in OnShown) {
+					action(this);
+				}
+			});
+		}
+	}
+}
--- ULTRAINTERFACE/src/Options.cs
+++ ULTRAINTERFACE/src/Options.cs
@@ -16,12 +16,12 @@ namespace ULTRAINTERFACE {
 		public static RectTransform OptionsMenu { get; internal set; } 
 		public static Transform GameplayOptionsContent { get; internal set; }
 
-		public static void CreateOptionsMenu(string title, Action<OptionsMenu> createAction, string buttonText = "", bool forceCaps = true) {
+		public static void CreateOptionsMenu(string title, Action<OptionsMenu> createAction, string buttonText = "", bool forceCaps = true, bool updateNavigation = true) {
 			UI.RegisterOnSceneLoad((scene) => {
 				OptionsMenu optionsMenu = CreateOptionsMenu_Internal(title, buttonText, forceCaps);
 
 				createAction(optionsMenu);
-				optionsMenu.Rebuild();
+				optionsMenu.Rebuild(updateNavigation);
 			});
 		}
 
@@ -110,8 +110,7 @@ namespace ULTRAINTERFACE {
 			});
 
 			optionsMenu.FirstShown.Add((menu) => {
-				menu.Rebuild();
-
+				menu.Rebuild(false); // Dont update navigation here, because it would overwrite any manual navigation that's been setup
 				menu.ScrollToTop(true);
 			});
 
@@ -211,136 +210,6 @@ namespace ULTRAINTERFACE {
 			RectTransform option = OptionsMenu.Find(optionName).GetComponent<RectTransform>();
 			option.SetParent(OptionsScroll.Content, false);
 			option.anchoredPosition = Vector2.zero;
-		}
-	}
-
-	public class OptionsMenu : UIComponent {
-		public List<Action<OptionsMenu>> LateCreate { get; private set; } = new List<Action<OptionsMenu>>();
-		public List<Action<OptionsMenu>> FirstShown { get; private set; } = new List<Action<OptionsMenu>>();
-		public List<Action<OptionsMenu>> OnShown { get; private set; } = new List<Action<OptionsMenu>>();
-
-		public CustomScrollView ScrollView { get; private set; }
-		public CustomButton OptionsButton { get; private set; }
-		public CustomText Title { get; private set; }
-
-		public RectTransform Content { get { return ScrollView.Content; } }
-
-		public bool IsInitalised { get; private set; } = false;
-		public bool HasBeenShown { get; private set; } = false;
-
-		float previousHeight;
-
-		public void ScrollToTop(bool includeChildren = true) {
-			if (!includeChildren) {
-				ScrollView.ScrollRect.ScrollToTop();
-				return;
-			}
-
-			foreach (ScrollRect scrollRect in GetComponentsInChildren<ScrollRect>(true)) {
-				scrollRect.ScrollToTop();
-			}
-		}
-
-		public RectTransform AddOptionsPanel(TextAnchor anchor = TextAnchor.MiddleLeft) {
-			return Options.CreateOptionsPanel(Content, anchor);
-		}
-
-		public CustomText AddHeader(string text, int size = 24, float width = 600, float height = 50, TextAnchor anchor = TextAnchor.MiddleCenter, bool forceCaps = true) {
-			return UI.CreateText(Content, text, size, width, height, anchor, forceCaps);
-		}
-
-		public CustomButton AddButton(string text, UnityAction onClick, int fontSize = 24, float width = 600, float height = 60, bool forceCaps = true) {
-			return UI.CreateButton(Content, text, onClick, fontSize, width, height, forceCaps);
-		}
-
-		public CustomButton AddButton(string text, int fontSize = 24, float width = 600, float height = 60, bool forceCaps = true) {
-			return UI.CreateButton(Content, text, fontSize, width, height, forceCaps);
-		}
-
-		public void Rebuild() {
-			Content.Rebuild();
-			LayoutRebuilder.ForceRebuildLayoutImmediate(Content);
-
-			// Content.anchoredPosition = new Vector2(Content.anchoredPosition.x, Content.anchoredPosition.y - (Content.sizeDelta.y - previousHeight) / 2);
-
-			UpdateNavigation();
-		}
-
-		void LateUpdate() {
-			previousHeight = Content.sizeDelta.y;
-		}
-
-		internal void Init(CustomScrollView scrollView, CustomButton optionsButton, CustomText title) {
-			if (IsInitalised) {
-				UI.Log.LogError($"Options Menu \"{gameObject.name}\" already initalised, returning...");
-				return;
-			}
-
-			this.ScrollView = scrollView;
-			this.OptionsButton = optionsButton;
-			this.Title = title;
-
-			this.HasBeenShown = false;
-
-			CoroManager.InvokeNextFrame(() => {
-				foreach (Action<OptionsMenu> action in LateCreate) {
-					action(this);
-				}
-				
-				Rebuild();
-			});
-
-			IsInitalised = true;
-		}
-
-		void UpdateNavigation() {
-			Selectable[] selectables = ScrollView.Content.GetComponentsInChildren<Selectable>(false);
-
-			for (int i = 0; i < selectables.Length; i++) {
-				Selectable selectable = selectables[i];
-
-				Navigation nav = new Navigation();
- 				nav.mode = Navigation.Mode.Explicit;
-
-				if (i > 0) {
-					nav.selectOnUp = selectables[i - 1];
-				} else {
-					nav.selectOnUp = selectables[selectables.Length - 1];
-				}
-				if (i < selectables.Length - 1) {
-					nav.selectOnDown = selectables[i + 1];
-				} else {
-					nav.selectOnDown = selectables[0];
-				}
-
-				selectable.navigation = nav;
-			}
-		}
-
-		public void SetTitle(string titleText, bool forceCaps = true) {
-			Title.SetText($"--{titleText}--", forceCaps);
-		}
-
-		public void SetButtonText(string buttonText, bool forceCaps = true) {
-			OptionsButton.Text.SetText(buttonText, forceCaps);
-		}
-
-		void OnEnable() {
-			if (!HasBeenShown) {
-				HasBeenShown = true;
-
-				CoroManager.InvokeNextFrame(() => {
-					foreach (Action<OptionsMenu> action in FirstShown) {
-						action(this);
-					}
-				});
-			}
-
-			CoroManager.InvokeNextFrame(() => {
-				foreach (Action<OptionsMenu> action in OnShown) {
-					action(this);
-				}
-			});
 		}
 	}
 }
